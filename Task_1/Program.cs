2899d61af6495ea0cecd4168f317e0560c03fd8f
NikolayGlazkov <gn9377419582@gmail.com>
2023-02-04 10:54:36 +0300

    Task_1 END

---
+++ Task_1/Program.cs
@@ -0,0 +1,29 @@
+﻿// Показать двумерный массив размером m×n заполненный вещественными числами
+// Задать двумерный массив следующим правилом: Aₘₙ = m+n
+// В двумерном массиве заменить элементы, у которых оба индекса чётные на их квадраты
+// В матрице чисел найти сумму элементов главной диагонали
+
+Console.WriteLine("введи число m количество строк");
+ int m = int.Parse(Console.ReadLine());
+Console.WriteLine("введи число n количество столбиков");
+ int n = int.Parse(Console.ReadLine());
+
+double[,] array = new double[m, n];
+
+for (int rows = 0; rows < m; rows++)
+{
+    for (int colums = 0; colums < n; colums++)
+    {
+    
+        array[rows, colums] = new Random().Next(1,10);;
+    }
+}
+
+for (int rows = 0; rows < m; rows++)
+{
+    for (int colums = 0; colums < n; colums++)
+    {
+        Console.Write($"{array[rows, colums]} ");
+    }
+    Console.WriteLine();
+}
